c968bb24d618ff305d623f0b9efbacd5a2be20e4
vanhhn <vietanhvmo@gmail.com>
2025-03-07 18:23:49 +0700

    sai te le

---
+++ OOP012.java
@@ -0,0 +1,134 @@
+import java.util.Scanner;
+
+class Author {
+  private String name, email, gender;
+
+  public Author(String name, String email, String gender) {
+    this.name = name;
+    this.email = email;
+    this.gender = gender;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String toString() {
+    return String.format("Author[name=%s, email=%s, gender=%s]", this.name, this.email, this.gender);
+  }
+}
+
+class Book {
+  private String name;
+  private Author author;
+  private String price; // Chuyển sang String
+  private int qty = 0;
+
+  public Book(String name, Author author, String price) {
+    this.name = name;
+    this.author = author;
+    this.price = price;
+  }
+
+  public Book(String name, Author author, String price, int qty) {
+    this.name = name;
+    this.author = author;
+    this.price = price;
+    this.qty = qty;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Author getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(Author author) {
+    this.author = author;
+  }
+
+  public String getPrice() {
+    return price;
+  }
+
+  public void setPrice(String price) {
+    this.price = price;
+  }
+
+  public int getQty() {
+    return qty;
+  }
+
+  public void setQty(int qty) {
+    this.qty = qty;
+  }
+
+  public String toString() {
+    return String.format("Book[name=%s, %s, price=%s, qty=%d]", this.name, this.author.toString(), this.price, this.qty);
+  }
+}
+
+public class OOP012 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    sc.nextLine();
+    while (t-- > 0) {
+      String s = sc.nextLine();
+      if (s.equals("Book")) {
+        String name_book = sc.nextLine();
+        String price = sc.nextLine(); // đổi từ double sang String
+        int so_luong = Integer.parseInt(sc.nextLine());
+        sc.nextLine(); // bỏ qua dòng Author
+        String name_author = sc.nextLine();
+        String email = sc.nextLine();
+        String gender = sc.nextLine();
+        Author author = new Author(name_author, email, gender);
+        Book book = new Book(name_book, author, price, so_luong);
+        System.out.println(book);
+        sc.nextLine(); // bỏ qua dòng End
+      } else if (s.equals("Author")) {
+        String name_author = sc.nextLine();
+        String email = sc.nextLine();
+        String gender = sc.nextLine();
+        sc.nextLine(); // bỏ qua dòng Book
+        Author author = new Author(name_author, email, gender);
+        String name_book = sc.nextLine();
+        String price = sc.nextLine(); // đổi từ double sang String
+        int so_luong = Integer.parseInt(sc.nextLine());
+        Book book = new Book(name_book, author, price, so_luong);
+        System.out.println(book);
+        sc.nextLine(); // bỏ qua dòng End
+      } else if (s.equals("End")) {
+        continue;
+      }
+    }
+    sc.close();
+  }
+}
