5ab26490f7ed191983455cfb4ecb5606a4b88113
vanhhn <vietanhvmo@gmail.com>
2025-03-09 20:32:41 +0700

    ok

--- COLLECTION006.java
+++ COLLECTION006.java
@@ -1,36 +1,35 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Main {
+public class COLLECTION006 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = Integer.parseInt(sc.nextLine());
-        while(t-- > 0){
-            String[] s =  sc.nextLine().split(" ");
-            Map<String,Integer> map = new HashMap<>();
-            for(int i = 0; i < s.length; i += 2){
+        while (t-- > 0) {
+            String[] s = sc.nextLine().split(" ");
+            Map<String, Integer> map = new HashMap<>();
+            for (int i = 0; i < s.length; i += 2) {
                 map.put(s[i], Integer.parseInt(s[i + 1]));
             }
             System.out.println(rarest(map));
         }
         sc.close();
     }
+
     public static int rarest(Map<String, Integer> map) {
         int[] a = new int[1000];
-        for(int value : map.values()){
+        for (int value : map.values()) {
             a[value]++;
         }
         int mn = map.size(), ans = 1000;
-        for(int value : map.values()){
+        for (int value : map.values()) {
             mn = Math.min(mn, a[value]);
         }
-        for(int value: map.values()){
-            if(mn == a[value])
-                ans = Math.min(ans,value); 
-            
+        for (int value : map.values()) {
+            if (mn == a[value])
+                ans = Math.min(ans, value);
+
         }
         return ans;
     }
