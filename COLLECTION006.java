1fae0becefa22484affa6c7acdb6abf2c6e98130
vanhhn <vietanhvmo@gmail.com>
2025-03-09 20:31:00 +0700

    dieu chinh code bai Collection 006

--- COLLECTION006.java
+++ COLLECTION006.java
@@ -1,39 +1,37 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class COLLECTION006 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int t = sc.nextInt();
-    sc.nextLine();
-    while (t-- > 0) {
-      HashMap<String, Integer> hm = new HashMap<>();
-      String s = sc.nextLine();
-      String[] ar = s.split("\\s+");
-      for (int i = 1; i < ar.length; i += 2) {
-        if (hm.containsKey(ar[i])) {
-          hm.put(ar[i], hm.get(ar[i]) + 1);
-        } else {
-          hm.put(ar[i], 1);
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while(t-- > 0){
+            String[] s =  sc.nextLine().split(" ");
+            Map<String,Integer> map = new HashMap<>();
+            for(int i = 0; i < s.length; i += 2){
+                map.put(s[i], Integer.parseInt(s[i + 1]));
+            }
+            System.out.println(rarest(map));
         }
-      }
-      // for (String x : hm.keySet()) {
-      //   System.out.println(x + " " + hm.get(x));
-      // }
-      int ans = 1000;
-      int cnt=1000;
-      for (Map.Entry<String, Integer> x : hm.entrySet()) {
-        if (cnt >x.getValue()) {
-          cnt=x.getValue();
-          ans=Integer.parseInt(x.getKey());
-        }
-        else if(cnt==x.getValue()){
-          ans=Math.min(ans, Integer.parseInt(x.getKey()));
-        }
-      }
-      System.out.println(ans);
+        sc.close();
     }
-    sc.close();
-  }
+    public static int rarest(Map<String, Integer> map) {
+        int[] a = new int[1000];
+        for(int value : map.values()){
+            a[value]++;
+        }
+        int mn = map.size(), ans = 1000;
+        for(int value : map.values()){
+            mn = Math.min(mn, a[value]);
+        }
+        for(int value: map.values()){
+            if(mn == a[value])
+                ans = Math.min(ans,value); 
+            
+        }
+        return ans;
+    }
 }
--- OOP012.java
+++ OOP012.java
@@ -41,16 +41,16 @@ class Author {
 class Book {
   private String name;
   private Author author;
-  private String price; // Chuyển sang String
+  private double price; // Chuyển sang String
   private int qty = 0;
 
-  public Book(String name, Author author, String price) {
+  public Book(String name, Author author, double price) {
     this.name = name;
     this.author = author;
     this.price = price;
   }
 
-  public Book(String name, Author author, String price, int qty) {
+  public Book(String name, Author author, double price, int qty) {
     this.name = name;
     this.author = author;
     this.price = price;
@@ -73,11 +73,11 @@ class Book {
     this.author = author;
   }
 
-  public String getPrice() {
+  public double getPrice() {
     return price;
   }
 
-  public void setPrice(String price) {
+  public void setPrice(double price) {
     this.price = price;
   }
 
@@ -90,7 +90,7 @@ class Book {
   }
 
   public String toString() {
-    return String.format("Book[name=%s, %s, price=%s, qty=%d]", this.name, this.author.toString(), this.price, this.qty);
+    return String.format("Book[name=%s, %s, price=%.2f, qty=%d]", this.name, this.author.toString(), this.price, this.qty);
   }
 }
 
@@ -103,7 +103,7 @@ public class OOP012 {
       String s = sc.nextLine();
       if (s.equals("Book")) {
         String name_book = sc.nextLine();
-        String price = sc.nextLine(); // đổi từ double sang String
+        double price = Double.parseDouble(sc.nextLine());
         int so_luong = Integer.parseInt(sc.nextLine());
         sc.nextLine(); // bỏ qua dòng Author
         String name_author = sc.nextLine();
@@ -111,23 +111,22 @@ public class OOP012 {
         String gender = sc.nextLine();
         Author author = new Author(name_author, email, gender);
         Book book = new Book(name_book, author, price, so_luong);
-        System.out.println(book);
         sc.nextLine(); // bỏ qua dòng End
-      } else if (s.equals("Author")) {
+        System.out.println(book);
+      } 
+      else if (s.equals("Author")) {
         String name_author = sc.nextLine();
         String email = sc.nextLine();
         String gender = sc.nextLine();
         sc.nextLine(); // bỏ qua dòng Book
         Author author = new Author(name_author, email, gender);
         String name_book = sc.nextLine();
-        String price = sc.nextLine(); // đổi từ double sang String
+       double price = Double.parseDouble(sc.nextLine()); // đổi từ double sang String
         int so_luong = Integer.parseInt(sc.nextLine());
         Book book = new Book(name_book, author, price, so_luong);
-        System.out.println(book);
         sc.nextLine(); // bỏ qua dòng End
-      } else if (s.equals("End")) {
-        continue;
-      }
+        System.out.println(book);   
+      } 
     }
     sc.close();
   }
