a38fe77d306c45329fed17a936d2c4e394fcc460
vanhhn <vietanhvmo@gmail.com>
2025-03-09 20:33:29 +0700

    code Basic006 ko co testcase

--- BASIC006.java
+++ BASIC006.java
@@ -1,25 +1,18 @@
-
-
 import java.util.Scanner;
 
-public class BASIC006 {
+public class BASIC006{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while(sc.hasNext()){
+            String  s1 = sc.nextLine();
+            int cnt=0;
+            for(int i = 0; i < s1.length(); ++i){
+                if(s1.charAt(i) >= '0' && s1.charAt(i) <= '9') cnt++;
+            }
+            System.out.println(cnt);
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-          Scanner sc=new Scanner(System.in);
-          int t=sc.nextInt();
-          sc.nextLine();
-          while(t-->0) {
-        	  String s=sc.nextLine();
-        	  int cnt=0;
-        	  for(int i=0;i<s.length();i++) {
-        		  if(s.charAt(i)>='0'&&s.charAt(i)<='9') {
-        			  cnt++;
-        		  }
-        	  }
-        	  System.out.println(cnt);
-          }
-          sc.close();
-	}
-
+        sc.close();
+    }
 }
